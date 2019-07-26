d2148d50f40b34c0f4df3cf2fca428b0de9c8535
ablin <space6fic@gmail.com>
2019-07-26 05:19:59 +0200

    implementing forms and pages to request my api

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const request = require('request-promise');
 
 // Load Own Modules
 const torrents = require('./api/torrents');
@@ -11,6 +12,9 @@ const popcorn = require('./api/popcorn');
 
 // Set Engine
 const app = express();
+
+// Set the view engine
+app.set('view engine', 'ejs');
 
 // Body Parser Middleware
 // Parse app/x-www-form-urlencoded
@@ -23,6 +27,27 @@ app.use('/api/torrents', torrents);
 app.use('/api/leetx', leetx);
 app.use('/api/yts', yts);
 app.use('/api/popcorn', popcorn);
+
+app.get('/', async (req, res) => {
+    top = await request('http://localhost:8089/api/torrents/top');
+    res.render('home', {
+            top: top
+    })
+    res.status(200);
+})
+
+app.post('/search', async (req, res) => {
+    const query = req.body.query,
+    page  = req.body.page,
+    type  = req.body.type,
+    order = req.body.order;
+    movies = await request(`http://localhost:8089/api/torrents/search/${query}/${page}/${type}/${order}`);
+
+  
+    res.status(200).render('searchresult', {
+        movies: movies
+    });
+})
 
 const port = process.env.PORT || 8089;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
